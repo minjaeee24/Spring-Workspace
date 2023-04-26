8c348bc180204ee5eeb3b88b3aff9bff917d72d2
minjaeee <ghkswo0624@naver.com>
2023-04-26 15:14:33 +0900

    Spring 1주차
    ㅇㅇ

---
+++ SpringProject/src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.kh.spring.member.controller;
+
+import com.kh.spring.member.model.vo.Member;
+
+public class MemberController {
+
+//	public void test() {
+//		Member m = new Member().builder().address("추가할 값").userId("dd").build();
+//	}
+	
+}
